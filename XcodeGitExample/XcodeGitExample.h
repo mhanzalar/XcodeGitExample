a614b73d43954a64f979c0ae23d59019525e2ddb
maryam rasheed <maryamrasheed@maryams-MacBook-Pro.local>
2017-09-13 13:02:48 -0500

    Initial Commit

---
+++ XcodeGitExample/XcodeGitExample.h
@@ -0,0 +1,19 @@
+//
+//  XcodeGitExample.h
+//  XcodeGitExample
+//
+//  Created by maryam rasheed on 9/13/17.
+//  Copyright © 2017 maryam rasheed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XcodeGitExample.
+FOUNDATION_EXPORT double XcodeGitExampleVersionNumber;
+
+//! Project version string for XcodeGitExample.
+FOUNDATION_EXPORT const unsigned char XcodeGitExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XcodeGitExample/PublicHeader.h>
+
+
